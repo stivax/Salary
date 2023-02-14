b83146efc0b74283e877268fc215a494827f5f52
stivax <stivax@gmail.com>
2023-02-15 00:30:05 +0200

    1.1

--- app/src/main/java/com/example/salary/MainActivity.java
+++ app/src/main/java/com/example/salary/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.salary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
+import android.view.WindowInsets;
 import android.widget.Button;
 import android.widget.EditText;
 
